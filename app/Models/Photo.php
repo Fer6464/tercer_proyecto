cc0b4f51a969fa643ba375218a8f8f00d42025a4
Fer6464 <ariasfernando966@gmail.com>
2025-06-05 17:58:49 -0500

    no funciono aca

---
+++ app/Models/Photo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Tag;
+
+class Photo extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'url'];
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+}
